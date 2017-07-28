ec46009bcd8aaddac8121c6c1b8509cf7ca2cc54
Ralph Luring <bennettchrislloyd@gmail.com>
2017-07-28 11:59:46 -0700

    service worker fix in index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceworker from './registerServiceworker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+registerServiceworker();
