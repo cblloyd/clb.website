5f2b5eccb179fbabcc4f28b8258578c07649f25a
Ralph Luring <bennettchrislloyd@gmail.com>
2017-08-02 23:47:28 -0700

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceworker from './registerServiceworker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceworker();
